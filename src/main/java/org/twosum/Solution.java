17b084662b5ac705c27a5d8075f3fe1018b96bd7
AlbertoPlaza <albertoplazamontesdm@gmail.com>
2025-04-10 00:42:15 +0200

    add: twosum solution

--- src/main/java/org/twosum/Solution.java
+++ src/main/java/org/twosum/Solution.java
@@ -1,2 +1,20 @@
-package org.twosum;public class Solution {
+package org.twosum;
+
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++){
+        int searchNum = target - nums[i];
+
+        if (map.containsKey(searchNum)) return new int[] {map.get(searchNum), i};
+        else map.put(nums[i], i);
+
+    }
+        return new int[] {};
+    }
 }
+
