3eac97df8e5728f73519d1b2c94ee97e152a03ed
AlbertoPlaza <albertoplazamontesdm@gmail.com>
2025-05-03 18:29:23 +0200

    add: Letter combinations of a phone number

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        org.twosum.Solution solution = new org.twosum.Solution();
+        org.searchinsert.Solution searchInsertSolution = new org.searchinsert.Solution();
+        org.palindromenumber.Solution palindromeSolution = new org.palindromenumber.Solution();
+        org.validparentheses.Solution validParenthesesSolution = new org.validparentheses.Solution();
+        org.reverseinteger.Solution reverseInteger = new org.reverseinteger.Solution();
+        org.lettercombinationsofaphonenumber.Solution letterComb = new org.lettercombinationsofaphonenumber.Solution();
+
+        int[] solTwoNum = solution.twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println("Solution: " + solTwoNum[0] + ", " + solTwoNum[1]);
+
+        int[] solTwoNum2 = solution.twoSum(new int[]{3, 2, 4}, 6);
+        System.out.println("Solution: " + solTwoNum2[0] + ", " + solTwoNum2[1]);
+        int solSearchInsert = searchInsertSolution.searchInsert(new int[]{1, 3, 5}, 6);
+        System.out.println("Solution: " + solSearchInsert);
+
+
+
+        boolean solPalindrome = palindromeSolution.isPalindrome(0);
+        System.out.println("Solution: " + solPalindrome);
+
+        boolean solValidParentheses = validParenthesesSolution.isValid("){)");
+        System.out.println("Solution: " + solValidParentheses);
+
+        int solReversedInteger = reverseInteger.reverse(123);
+        System.out.println("Solution " + solReversedInteger);
+
+        List<String> solLetterCombinations = letterComb.letterCombinations("2347");
+        System.out.println("Solution " + solLetterCombinations.toString());
     }
 }
